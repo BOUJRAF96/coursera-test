0ba7da74af4614fe7bfed267bbe51305615cbe8c
Mohamed BOUJRAF <m.boujraf.96@gmail.com>
2020-11-02 23:26:16 +0100

    mod2_solution

--- AngularJS/module1-solution/js/app.js
+++ AngularJS/module1-solution/js/app.js
@@ -1,30 +1,60 @@
+// (function(){
+//   'use strict';
+//   angular.module('LunchCheck', [])
+//   .controller('LunchCheckController', LunchCheckController);
+//   LunchCheckController.$inject = ['$scope'];
+//   function LunchCheckController($scope){
+//     $scope.dishes = '';
+//     $scope.message = '';
+//     $scope.checked = false;
+//
+//     $scope.checkLunch = function() {
+//       if ($scope.dishes.trim().length === 0) {
+//           $scope.empty = true;
+//       } else {
+//           $scope.checked = true;
+//           $scope.empty = false;
+//           var arrayDishes = $scope.dishes.split(',');
+//           var arrayDishesWithoutEmptys = arrayDishes.filter(function(v) {
+//               return v.trim() !== '';
+//           });
+//
+//           if (arrayDishesWithoutEmptys.length <= 3) {
+//               $scope.message = 'Enjoy!';
+//           } else {
+//               $scope.message = 'Too much!';
+//           }
+//       }
+//     };
+//   };
+// })();
 (function(){
-  'use strict';
-  angular.module('LunchCheck', [])
-  .controller('LunchCheckController', LunchCheckController);
-  LunchCheckController.$inject = ['$scope'];
-  function LunchCheckController($scope){
-    $scope.dishes = '';
-    $scope.message = '';
-    $scope.checked = false;
+'use strict';
+angular.module('LunchCheck',[])
+.controller('LunchCheckController',LunchCheckController);
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope){
+  $scope.message="";
+  $scope.state="";
+  $scope.state2="";
+  $scope.checkLunch=function () {
 
-    $scope.checkLunch = function() {
-      if ($scope.dishes.trim().length === 0) {
-          $scope.empty = true;
-      } else {
-          $scope.checked = true;
-          $scope.empty = false;
-          var arrayDishes = $scope.dishes.split(',');
-          var arrayDishesWithoutEmptys = arrayDishes.filter(function(v) {
-              return v.trim() !== '';
-          });
+if($scope.dishes===undefined||$scope.dishes===""){
+  $scope.message="please Enter Data First";
+    $scope.state="red";
+      $scope.state2="red";
+}
+else {
+  let commaCal=$scope.dishes.split(",").length;
+  if (commaCal>=1 && commaCal<=3) {
+    $scope.message="Enjoy";
 
-          if (arrayDishesWithoutEmptys.length <= 3) {
-              $scope.message = 'Enjoy!';
-          } else {
-              $scope.message = 'Too much!';
-          }
-      }
-    };
-  };
+  }else {
+    $scope.message="Too much";
+}
+  $scope.state="green";
+    $scope.state2="green";
+}
+}
+};
 })();
